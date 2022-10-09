a0fb6a072e1d48932287c99290d69db4607b5853
HHuzaifa <huzaifa50100@gmail.com>
2022-10-09 19:21:54 +0500

    Famous person and their quote exercise completed.

---
+++ Famous Quote 2/famousQuote2.js
@@ -0,0 +1,5 @@
+let famousPerson = "Nikola Tesla";
+let famouseQuote = "Be alone, that is the secret in invention; be alone, that is when ideas are born";
+
+const message = `${famousPerson} once said, ${famouseQuote}.`;
+console.log(message);
