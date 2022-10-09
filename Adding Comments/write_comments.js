3bb78d73bcbeccace11406af6bab3eb1ef6301d4
HHuzaifa <huzaifa50100@gmail.com>
2022-10-09 23:05:42 +0500

    Wrote comments in two of the programs.

---
+++ Adding Comments/write_comments.js
@@ -0,0 +1,11 @@
+//Hi my name is Huzaifa and, the code below displays my favourite number stored in a variable.
+let favourite_number = 5;
+console.log(`My favourite number is ${favourite_number}.`);
+
+
+//Stored a famous person's name and a quote of him in a variable and displayed a message.
+let famousPerson = "Nikola Tesla";
+let famouseQuote = "Be alone, that is the secret in invention; be alone, that is when ideas are born";
+
+const message = `${famousPerson} once said, ${famouseQuote}.`;
+console.log(message);
