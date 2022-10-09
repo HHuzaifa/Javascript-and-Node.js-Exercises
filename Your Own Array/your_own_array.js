a4daba22cc25bebf6a9160a2f141e4e305dca207
HHuzaifa <huzaifa50100@gmail.com>
2022-10-10 01:22:15 +0500

    .

---
+++ Your Own Array/your_own_array.js
@@ -0,0 +1,5 @@
+favMotobikeModels = ["honda", "triumph", "royal enfield", "suzuki", "kawasaki"];
+console.log(`I would like to own a ${favMotobikeModels[0]} motocycle someday.`);
+console.log(`I would like to own a ${favMotobikeModels[1]} motocycle someday.`);
+console.log(`I would like to own a ${favMotobikeModels[2]} motocycle someday.`);
+console.log(`I would like to own a ${favMotobikeModels[3]} motocycle someday.`);
