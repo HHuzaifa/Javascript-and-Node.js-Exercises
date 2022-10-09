f435ef5adf5e5cd049b998e0bd6f04b93a2845ef
HHuzaifa <huzaifa50100@gmail.com>
2022-10-10 01:38:22 +0500

    Displayed an invitation message to # dear relatives.

---
+++ Guest List/guest_list.js
@@ -0,0 +1,7 @@
+guestList = ["mom", "brother", "father"];
+
+console.log(`Hi ${guestList[0]}, I'll feel realy happy if you would come over \
+and have a dinner with me.`);
+console.log(`Hi ${guestList[1]}, I'm having dinner at home today, would you join me?`);
+console.log(`Hi ${guestList[2]}, It's been a long time since we had a meal together,\
+ would you come over tonight to have a dinner?`);
