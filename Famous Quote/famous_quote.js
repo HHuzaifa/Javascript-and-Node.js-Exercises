96f2a227641eb6c800d7611d32354112f6755ea5
HHuzaifa <huzaifa50100@gmail.com>
2022-10-09 16:10:39 +0500

    commit

---
+++ Famous Quote/famous_quote.js
@@ -0,0 +1,3 @@
+let famousPerson = "Nikola Tesla"
+let famousQuote = "Be alone, that is the secret in invention; be alone, that is when ideas are born"
+console.log(`${famousPerson} once said, "${famousQuote}".`)
