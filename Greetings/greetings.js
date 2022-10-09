bc6176f9d8dca98e3ff74ea813ab010d690ee39d
HHuzaifa <huzaifa50100@gmail.com>
2022-10-10 00:03:03 +0500

    Displayed greeting separately to names stored in an array.

---
+++ Greetings/greetings.js
@@ -0,0 +1,6 @@
+names = ["jabbar", "steve", "jordan", "abraiz"];
+
+console.log(`Hi ${names[0]}, how are you doing today?`);
+console.log(`Hi ${names[1]}, how are you doing today?`);
+console.log(`Hi ${names[2]}, how are you doing today?`);
+console.log(`Hi ${names[3]}, how are you doing today?`);
