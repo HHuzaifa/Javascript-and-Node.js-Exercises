02d620a0ea22fa2ce228f8c5502ef6869985d1b8
Huzaifa Khan <90773074+HHuzaifa@users.noreply.github.com>
2022-10-09 16:19:29 +0500

    Delete Famous Quote directory

--- Famous Quote/famous_quote.js
+++
@@ -1,3 +0,0 @@
-let famousPerson = "Nikola Tesla"
-let famousQuote = "Be alone, that is the secret in invention; be alone, that is when ideas are born"
-console.log(`${famousPerson} once said, "${famousQuote}".`)
