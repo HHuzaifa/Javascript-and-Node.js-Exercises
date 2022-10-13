7a054409e25c494b8896b59b228c0874f1d57aac
HHuzaifa <huzaifa50100@gmail.com>
2022-10-14 02:48:57 +0500

    .

---
+++ Conditional Tests/conditional_tests.js
@@ -0,0 +1,16 @@
+let carArray = ["lamborghini", "suzuki", "bentley"];
+let codingIde = "visual studio code";
+let myName = "huzaifa";
+let myFavouriteLanguages = ["python", "solidity", "typescript", "javascript"]
+
+console.log("Does carArray have '3' items, I predict ture");
+console.log(carArray.length == 3);
+
+console.log("Is codingIde == 'visual studio code', I predict true");
+console.log(codingIde == "visual studio code");
+
+console.log("Is myName == 'huzaifa khan', I predict true");
+console.log(myName != "huzaifa khan");
+
+console.log("Does myFavouriteLanguages contains 4 items, I predict true.");
+console.log(myFavouriteLanguages.length == 4);
