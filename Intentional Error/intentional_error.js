f739161fb0406637465b32652f365b16c6b86e45
HHuzaifa <huzaifa50100@gmail.com>
2022-10-14 02:26:36 +0500

    .

---
+++ Intentional Error/intentional_error.js
@@ -0,0 +1,7 @@
+favMotobikeModels = ["honda", "triumph", "royal enfield", "suzuki", "kawasaki"];
+console.log(`I would like to own a ${favMotobikeModels[0]} motocycle someday.`);
+console.log(`I would like to own a ${favMotobikeModels[1]} motocycle someday.`);
+console.log(`I would like to own a ${favMotobikeModels[2]} motocycle someday.`);
+console.log(`I would like to own a ${favMotobikeModels[5]} motocycle someday.`);/*errored index that returned
+undefined instead of item in array*/
+console.log(`I would like to own a ${favMotobikeModels[3]} motocycle someday.`);//corrected index
