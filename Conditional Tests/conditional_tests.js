aa6dea61c5a0cd8f641ed97105df8c5504ed4383
HHuzaifa <huzaifa50100@gmail.com>
2022-10-14 03:02:01 +0500

    .

--- Conditional Tests/conditional_tests.js
+++ Conditional Tests/conditional_tests.js
@@ -2,8 +2,16 @@ let carArray = ["lamborghini", "suzuki", "bentley"];
 let codingIde = "visual studio code";
 let myName = "huzaifa";
 let myFavouriteLanguages = ["python", "solidity", "typescript", "javascript"]
+let brotherName = "hassan"
 
-console.log("Does carArray have '3' items, I predict ture");
+let myLaptopBrand = "dell";
+let myAge = 21;
+let imCoding = true;
+let friendName = "abraiz";
+let myHobby = "reading books"
+
+//true statements
+console.log("Does carArray have '3' items, I predict true");
 console.log(carArray.length == 3);
 
 console.log("Is codingIde == 'visual studio code', I predict true");
@@ -14,3 +22,22 @@ console.log(myName != "huzaifa khan");
 
 console.log("Does myFavouriteLanguages contains 4 items, I predict true.");
 console.log(myFavouriteLanguages.length == 4);
+
+console.log("Is brotherName == 'hassan', I predict true");
+console.log(brotherName == "hassan");
+
+//false statements
+console.log("is myLaptop Brand == 'lenovo', I predict false");
+console.log(myLaptopBrand == "lenovo");
+
+console.log("is myAge == 42, I predict false");
+console.log(myAge == 42);
+
+console.log("is friendName == 'zakir', I predict false");
+console.log(friendName == "zakir");
+
+console.log("is imCoding == false, I predict false");
+console.log(imCoding == false);
+
+console.log("is myHobby == 'reading books', I predict false");
+console.log(myHobby == "cricket")
