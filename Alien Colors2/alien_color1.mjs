8ce7c03ca5ab6f563758d35ad738c3b5529f2c8d
HHuzaifa <huzaifa50100@gmail.com>
2022-10-14 19:40:40 +0500

    .

---
+++ Alien Colors2/alien_color1.mjs
@@ -0,0 +1,8 @@
+let alienColor = "green";
+
+if (alienColor === "green") {
+    console.log("The player just earned 5 points for shooting the alien.")
+}
+if (alienColor !== "green") {
+    console.log("The player just earned 10 points.")
+}
