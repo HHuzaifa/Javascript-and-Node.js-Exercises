5d659da8f9183c0b27467ab134b9492a6afa3029
HHuzaifa <huzaifa50100@gmail.com>
2022-10-14 23:05:53 +0500

    .

--- Favourite Fruit/favourite_fruits.mjs
+++ Favourite Fruit/favourite_fruits.mjs
@@ -1,2 +1,17 @@
-let myFavouriteFruits = ["mango", "apple", "banana", "grapes", "avocado", "cherry"];
+let favouriteFruits = ["mango", "banana", "grapes"];
 
+if (favouriteFruits[0] === "mango") {
+    console.log(`You really like ${favouriteFruits[0]}s.`)
+}
+if (favouriteFruits[1] === "banana") {
+    console.log(`You really like ${favouriteFruits[1]}s.`)
+}
+if (favouriteFruits[2] === "grapes") {
+    console.log(`You really like ${favouriteFruits[2]}.`)
+}
+if (favouriteFruits[0] === "avocado") {
+    console.log(`You really like ${favouriteFruits[0]}'s.`)
+}
+if (favouriteFruits[2] === "cherries") {
+    console.log(`You really like ${favouriteFruits[0]}.`)
+}
