e2863fea2affd72a983d29d89e6e8538439f7521
HHuzaifa <huzaifa50100@gmail.com>
2022-10-16 01:49:50 +0500

    .

---
+++ Pizzas/pizzas.mjs
@@ -0,0 +1,6 @@
+let favouritePizzas = ["fajita", "chicken tikka", "creamy olives"];
+
+for (let i = 0; i < favouritePizzas.length; i++) {
+    console.log(`I like ${favouritePizzas[i]} pizza!`)
+}
+console.log("\nI really love pizzas")
