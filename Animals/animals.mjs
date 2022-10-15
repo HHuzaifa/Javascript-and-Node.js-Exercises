6c79c48e13ab58814747af37372caf8e12bc1353
HHuzaifa <huzaifa50100@gmail.com>
2022-10-16 02:58:06 +0500

    .

---
+++ Animals/animals.mjs
@@ -0,0 +1,6 @@
+let sameAnimals = ["tiger", "cat", "leopard"];
+
+for (let i = 0; i < sameAnimals.length; i++) {
+    console.log(`A ${sameAnimals[i]} would make a great pet haha.`)
+}
+console.log("All these animals belongs to an animal familed names as Felidae!")
