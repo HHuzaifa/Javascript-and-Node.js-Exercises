cc8453183416caf4c00c067a675dcaff1ef56b20
HHuzaifa <huzaifa50100@gmail.com>
2022-10-16 03:52:44 +0500

    .

--- T-Shirt/t_shirt.js
+++ T-Shirt/t_shirt.js
@@ -1,6 +1,8 @@
 function makeShirt(size, message) {
     let takeSize = prompt("Enter the size of the T-Shirt.")
+    takeSize = size;
     let takeMessage = prompt("Enter the message you want to be printed on your T-Shirt.")
-    console.log(`T-Shirt's size is ${toString(takeSize)}, and the message is ${toString(takeMessage)}.`)
+    takeMessage = message
+    console.log(`T-Shirt's size is ${takeSize}, and the message is ${takeMessage}.`)
 }
 makeShirt("extra large", "Danger Dude");
