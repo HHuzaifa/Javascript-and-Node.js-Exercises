54de1e92678239b74a2e1e4ae1705e2d412219ed
HHuzaifa <huzaifa50100@gmail.com>
2022-10-15 14:00:50 +0500

    .

---
+++ No Users/no_users.mjs
@@ -0,0 +1,8 @@
+let userNames = ["theprofessor", "bond007", "upworking43", "hassan43", "admin"];
+
+if (userNames.length != 0) {
+    userNames.length = 0
+}
+if (userNames.length == 0) {
+    console.log("We need to find some users!")
+}
