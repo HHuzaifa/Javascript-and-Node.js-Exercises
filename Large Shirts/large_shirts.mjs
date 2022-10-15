3ec2f37ece0b8ceeb8c37a45ac618f4e5b49a36a
HHuzaifa <huzaifa50100@gmail.com>
2022-10-16 04:36:45 +0500

    .

---
+++ Large Shirts/large_shirts.mjs
@@ -0,0 +1,14 @@
+function makeShirt(size, message) {
+    let shirtSize1 = "large";
+    let shirtSize2 = "medium";
+    let shirtMessage1 = "I love Javascript";
+
+    let shirtSize3 = "small";
+    let shirtMessage2 = "I love Typescript"
+    console.log(`Making ${shirtSize1} t-shirt with the message: ${shirtMessage1} printed on it.`);
+    console.log(`Making ${shirtSize2} t-shirt with the message: ${shirtMessage1} printed on it.`);
+    console.log(`Making ${shirtSize3} t-shirt with the message: ${shirtMessage2} printed on it.`);
+
+}
+
+makeShirt();
