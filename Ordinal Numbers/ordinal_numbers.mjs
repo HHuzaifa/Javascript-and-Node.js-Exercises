43545d9cf3b2ef4002cb35867306e91652252d1a
HHuzaifa <huzaifa50100@gmail.com>
2022-10-16 01:31:44 +0500

    .

--- Ordinal Numbers/ordinal_numbers.mjs
+++ Ordinal Numbers/ordinal_numbers.mjs
@@ -1,16 +1,16 @@
-let ordinalNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+let ordinalNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 let st = "st";
 let nd = "nd";
 let rd = "rd";
 let th = "th"
-for (let i  = 0; i < ordinalNumbers.length; i++) {
+for (let i  = 1; i < ordinalNumbers.length; i++) {
     if (i === ordinalNumbers[0]) {
         console.log(`${ordinalNumbers[0]}${st}`)
     }else if (i === ordinalNumbers[1]) {
         console.log(`${ordinalNumbers[1]}${nd}`)
     }else if(i === ordinalNumbers[2]) {
         console.log(`${ordinalNumbers[2]}${rd}`)
-    }else {
+    }if (i >= 4) {
         console.log(`${i}${th}`)
     }
 }
