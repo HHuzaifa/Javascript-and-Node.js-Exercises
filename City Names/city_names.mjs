27635f6c1d38b387ae1491adeeae19094b16f2b7
HHuzaifa <huzaifa50100@gmail.com>
2022-10-17 03:35:38 +0500

    .

---
+++ City Names/city_names.mjs
@@ -0,0 +1,8 @@
+function cityCountry(cityName, countryName) {
+    let message = `${cityName}, ${countryName}`
+    return message
+}
+
+console.log(cityCountry("London", "England"));
+console.log(cityCountry("Mumbai", "India"));
+console.log(cityCountry("Lahore", "Pakistan"));
