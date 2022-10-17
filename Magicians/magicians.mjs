d07c17036a0b6a537238c9e37363aef79f02f88c
HHuzaifa <huzaifa50100@gmail.com>
2022-10-18 03:07:12 +0500

    .

---
+++ Magicians/magicians.mjs
@@ -0,0 +1,8 @@
+function showMNames(namesList) {
+for ( let i = 0; i < namesList.length; i++) {
+    console.log(namesList[i])
+}
+}
+
+let magiciansNames = ["elliot", "james", "howlet", "stacy", "ruvenald"];
+showMNames(magiciansNames);
