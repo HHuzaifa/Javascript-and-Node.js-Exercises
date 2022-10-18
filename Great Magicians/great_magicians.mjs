0cb15d7c4ad61bfd36a8982ccf995ea6891b2f18
HHuzaifa <huzaifa50100@gmail.com>
2022-10-19 00:58:18 +0500

    .

---
+++ Great Magicians/great_magicians.mjs
@@ -0,0 +1,9 @@
+function showMNames(namesList) {
+    for ( let i = 0; i < namesList.length; i++) {
+        console.log(namesList[i])
+    }
+    }
+    
+    let magiciansNames = ["elliot", "james", "howlet", "stacy", "ruvenald"];
+    showMNames(magiciansNames);
+    
